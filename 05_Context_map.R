60fdf1deabba813905056a4edb9eeeb09db6d291
Laldinchhuaha Khiangte <164391897+DinosKuri@users.noreply.github.com>
2026-07-02 16:40:51 +0530

    Update 05_Context_map.R

--- 05_Context_map.R
+++ 05_Context_map.R
@@ -11,7 +11,7 @@
 # information used throughout the manuscript.
 #
 # Author:
-# DinosKuri Laldinchhuaha Khiangte
+# Laldinchhuaha Khiangte
 #
 # Repository:
 # https://github.com/DinosKuri/mizoram-extreme-rainfall-attribution
